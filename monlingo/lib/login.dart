8ba0937f6428341d9671268d9894d18fd02e6761
DarkQG <sw23d001@mandakh.edu.mn>
2024-06-05 11:47:17 +0800

    Update login.dart

--- monlingo/lib/login.dart
+++ monlingo/lib/login.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class login extends StatelessWidget {
+  const login({super.key});
 
   @override
   Widget build(BuildContext context) {
