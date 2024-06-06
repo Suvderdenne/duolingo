be006e96680d4d5a0f35ff32d0178af138e0d930
zndra <112457204+zndra@users.noreply.github.com>
2024-06-06 12:37:49 +0800

    ewwergergrweg

--- monlingo/lib/header_footer.dart
+++ monlingo/lib/header_footer.dart
@@ -2,7 +2,22 @@ import 'package:flutter/material.dart';
 import 'duo.dart';
 import 'timelapse.dart';
 import 'login.dart';
-class Footer extends StatelessWidget {
+
+class Footer extends StatefulWidget {
+  @override
+  _FooterState createState() => _FooterState();
+}
+
+class _FooterState extends State<Footer> {
+  // Variable to keep track of whether a button is pressed
+  Map<IconData, bool> _buttonPressed = {
+    Icons.home: false,
+    Icons.shop: false,
+    Icons.person_outlined: false,
+    Icons.timelapse: false,
+    Icons.more_horiz: false,
+  };
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -41,7 +56,10 @@ class Footer extends StatelessWidget {
     );
   }
 
-  Widget _buildFooterButton(BuildContext context, {required IconData icon, required Widget destination}) {
+  Widget _buildFooterButton(BuildContext context,
+      {required IconData icon, required Widget destination}) {
+    bool isPressed = _buttonPressed[icon] ?? false;
+
     return Material(
       elevation: 10,
       shape: CircleBorder(),
@@ -53,12 +71,21 @@ class Footer extends StatelessWidget {
             MaterialPageRoute(builder: (context) => destination),
           );
         },
+        onTapDown: (_) {
+          _updateButtonColor(icon, true);
+        },
+        onTapCancel: () {
+          _updateButtonColor(icon, false);
+        },
+        onTapUp: (_) {
+          _updateButtonColor(icon, false);
+        },
         child: Container(
           width: 56,
           height: 56,
           decoration: BoxDecoration(
             shape: BoxShape.circle,
-            color: Colors.white,
+            color: isPressed ? Colors.blue : Colors.white,
             boxShadow: [
               BoxShadow(
                 color: Colors.black.withOpacity(0.2),
@@ -75,5 +102,11 @@ class Footer extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  void _updateButtonColor(IconData icon, bool isPressed) {
+    setState(() {
+      _buttonPressed[icon] = isPressed;
+    });
   }
 }
