b068171ec0b98fc0875d65c7542663d48b23ed4c
tuguldur <kay2002man@gmail.com>
2024-06-19 12:11:37 +0800

    adcdavadvfdvdsavc

--- monlingo/lib/home.dart
+++ monlingo/lib/home.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:login_flutter/Pages/profile.dart';
 import 'package:login_flutter/start.dart';
 import 'header_footer.dart';
 import 'duo.dart';
