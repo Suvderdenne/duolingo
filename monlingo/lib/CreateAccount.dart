f284fc50714eb8716a763b01cfa1d8bfd2c36cb5
Boldkhuyg <116537426+Boldkhuyg@users.noreply.github.com>
2024-06-24 17:35:49 +0800

    createAccount zasaw

--- monlingo/lib/CreateAccount.dart
+++ monlingo/lib/CreateAccount.dart
@@ -3,8 +3,6 @@ import 'package:login_flutter/home.dart';
 import 'package:login_flutter/login.dart';
 import 'package:login_flutter/signup.dart';
 import 'package:lottie/lottie.dart';
-//import 'home.dart';
-//import 'signup.dart';
 
 void main() {
   runApp(MyApp());
@@ -27,6 +25,7 @@ class startpage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
@@ -34,13 +33,31 @@ class startpage extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               SizedBox(height: 20),
-              Lottie.asset("../assets/duo.json", width: 200),
+              Container(
+                width: 200,
+                height: 200,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  border: Border.all(
+                    color: Colors.purple,
+                    width: 4,
+                  ),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.asset(
+                    "../assets/home/logo.png",
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
               Spacer(),
               Text(
                 'Time to Create a Profile!',
                 style: TextStyle(
                   fontSize: 24,
                   fontWeight: FontWeight.bold,
+                  color: Colors.white, // Changed text color to white
                 ),
                 textAlign: TextAlign.center,
               ),
@@ -49,6 +66,7 @@ class startpage extends StatelessWidget {
                 'Create a profile to save your progress and continue learning for free.',
                 style: TextStyle(
                   fontSize: 16,
+                  color: Colors.white, // Changed text color to white
                 ),
                 textAlign: TextAlign.center,
               ),
@@ -56,13 +74,14 @@ class startpage extends StatelessWidget {
               Column(
                 children: [
                   ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => HomePage()));
-                      },
-                      child: Text("Home")),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => HomePage()),
+                      );
+                    },
+                    child: Text("Home"),
+                  ),
                   SizedBox(
                     width: double.infinity,
                     child: ElevatedButton(
@@ -79,7 +98,7 @@ class startpage extends StatelessWidget {
                         ),
                       ),
                       style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.green,
+                        backgroundColor: Colors.purple,
                         padding: EdgeInsets.symmetric(vertical: 16),
                       ),
                     ),
@@ -96,6 +115,8 @@ class startpage extends StatelessWidget {
                       },
                       child: Text('Already have an account?'),
                       style: TextButton.styleFrom(
+                        shadowColor:
+                            Colors.white, // Changed text color to white
                         padding: EdgeInsets.symmetric(vertical: 16),
                       ),
                     ),
--- monlingo/lib/duo.dart
+++ monlingo/lib/duo.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:login_flutter/header_footer.dart';
 
-
 void main() {
   runApp(MyApp());
 }
@@ -22,216 +21,239 @@ class Duo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-          title: 
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            
-             children: [
-              Spacer(),
-              Padding(
-                padding: const EdgeInsets.only(left: 25.0), // Зүүн талд 40 пикселийн зай нэмнэ
-                child:Row(
-                  children: [
-                    Icon(Icons.favorite, color: Colors.red),
-                    SizedBox(width: 5), // Текст болон дүрс хоорондын зай нэмнэ
-                    Text('5'),
-                  ],
-                ),
-              ),
-             ],
-          ),
-          bottom: PreferredSize(
-            preferredSize: Size.fromHeight(4.0), // LinearProgressIndicator-ийн өндрийг тохируулна
-            child: LinearProgressIndicator(
-              value: 0.5,
-              valueColor: AlwaysStoppedAnimation<Color>(Colors.green),
-              backgroundColor: Colors.grey[200],
-            ),
-          ),
-        ),
-      body: Padding(
-        padding: EdgeInsets.all(10),
-        child:SingleChildScrollView(
-        child: Column(
-          children: [              
-            Row(
-               mainAxisAlignment: MainAxisAlignment.start,
-               children: [
-                Padding(
-                padding: const EdgeInsets.only(left: 20.0),
-                 child: Text(
-                  "NEW WORD",
-                  style: TextStyle(
-                    fontSize: 15,
-                    color: Color.fromARGB(255, 231, 17, 184),
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-               ],
-            ), 
-            Row(
-               mainAxisAlignment: MainAxisAlignment.start,
-               children: [
-                Padding(
-                padding: const EdgeInsets.only(left: 20.0),
-                 child: Text(
-                  "Which of these is",
-                  style: TextStyle(
-                    fontSize: 22,
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-               ],
-            ),   
-             SizedBox(height: 25,),    
-              Row( 
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [ ElevatedButton(onPressed: () { 
-              }, 
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.white,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(2),
-                  side: BorderSide(
-                    color: Color.fromARGB(255, 167, 163, 163),
-                    width: 1
-                  ),
-                ),
-                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 20)
-              ),
-            
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Spacer(),
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: 25.0), // Зүүн талд 40 пикселийн зай нэмнэ
               child: Row(
                 children: [
-                  Column(
-                    children: [
-                     Image.asset("../assets/duo/la_donna.png", width: 100, height: 150,),
-                      SizedBox(width: 20),
-                      Text("la donna",
-                      style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.black
-                      ),),],
-                  ),                  
-                ],),
-              ),
-              SizedBox(width: 20),
-              ElevatedButton(onPressed: (){
-              }, 
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.white,
-                shape: RoundedRectangleBorder(
-                  // borderRadius: BorderRadius.circular(2),
-                  side: BorderSide(
-                  color: Color.fromARGB(255, 167, 163, 163),
-                    width: 1
-                  ),
-                ),
-                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 20)
-              ),
-              child: Row(
-                children: [
-                  Column(
-                    children: [
-                      Image.asset("../assets/duo/luomo.png", width: 100, height: 150,),
-                      Text("l'uomo",
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.black
-                  ),),
-                    ],
-                  ),
+                  Icon(Icons.favorite, color: Colors.red),
+                  SizedBox(width: 5), // Текст болон дүрс хоорондын зай нэмнэ
+                  Text('5'),
                 ],
-              )),
-              ],
-            ),
-                  
-            
-            SizedBox(height: 20,),
-            
-             Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [ ElevatedButton(onPressed: () { 
-              }, 
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.white,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(2),
-                  side: BorderSide(
-                  color: Color.fromARGB(255, 167, 163, 163),
-                  width: 1
-                  ),
-                ),
-                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 20)
               ),
-              child: Row(
-                children: [
-                  Column(
-                    children: [
-                      Image.asset("../assets/duo/il_ragazzo.png",  width: 100, height: 150,),
-                      Text("il ragazzo",
-                      style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.black
-                      ),),],
-                  ),                  
-                ],),
-              ),
-              SizedBox(width: 20),
-              ElevatedButton(onPressed: (){
-              }, 
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.white,
-                shape: RoundedRectangleBorder(
-                  // borderRadius: BorderRadius.circular(2),
-                  side: BorderSide(
-                    color: Color.fromARGB(255, 167, 163, 163),
-                    width: 1
-                  ),
-                ),
-                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 20)
-              ),
-              child: Row(
-                children: [
-                  Column(
-                    children: [
-                      Image.asset("../assets/duo/la_ragazza.png", width: 100, height: 150,),
-                      Text("la ragazza",
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.black
-                  ),),
-                    ],
-                  ),
-                ],
-              )),
-              ],
             ),
-          SizedBox(height: 60,),
-           ElevatedButton(onPressed: () {            
-          }, 
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Color.fromARGB(255, 148, 228, 148),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(5)
-            ),
-            minimumSize: Size(200, 50),
-          ),
-          child: 
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("CHECK", style: TextStyle(fontSize: 20, color: Color.fromARGB(255, 15, 15, 15)),)
-            ],
-          ) ),
           ],
         ),
-        
-      ),),
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(
+              4.0), // LinearProgressIndicator-ийн өндрийг тохируулна
+          child: LinearProgressIndicator(
+            value: 0.5,
+            valueColor: AlwaysStoppedAnimation<Color>(Colors.green),
+            backgroundColor: Colors.grey[200],
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20.0),
+                    child: Text(
+                      "NEW WORD",
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Color.fromARGB(255, 231, 17, 184),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20.0),
+                    child: Text(
+                      "Which of these is",
+                      style: TextStyle(
+                        fontSize: 22,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(2),
+                          side: BorderSide(
+                              color: Color.fromARGB(255, 167, 163, 163),
+                              width: 1),
+                        ),
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 15, vertical: 20)),
+                    child: Row(
+                      children: [
+                        Column(
+                          children: [
+                            Image.asset(
+                              "../assets/duo/la_donna.png",
+                              width: 100,
+                              height: 150,
+                            ),
+                            SizedBox(width: 20),
+                            Text(
+                              "la donna",
+                              style:
+                                  TextStyle(fontSize: 20, color: Colors.black),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(width: 20),
+                  ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white,
+                          shape: RoundedRectangleBorder(
+                            // borderRadius: BorderRadius.circular(2),
+                            side: BorderSide(
+                                color: Color.fromARGB(255, 167, 163, 163),
+                                width: 1),
+                          ),
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 15, vertical: 20)),
+                      child: Row(
+                        children: [
+                          Column(
+                            children: [
+                              Image.asset(
+                                "../assets/home/logo.png",
+                                width: 100,
+                                height: 150,
+                              ),
+                              Text(
+                                "l'uomo",
+                                style: TextStyle(
+                                    fontSize: 20, color: Colors.black),
+                              ),
+                            ],
+                          ),
+                        ],
+                      )),
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(2),
+                          side: BorderSide(
+                              color: Color.fromARGB(255, 167, 163, 163),
+                              width: 1),
+                        ),
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 15, vertical: 20)),
+                    child: Row(
+                      children: [
+                        Column(
+                          children: [
+                            Image.asset(
+                              "../assets/duo/il_ragazzo.png",
+                              width: 100,
+                              height: 150,
+                            ),
+                            Text(
+                              "il ragazzo",
+                              style:
+                                  TextStyle(fontSize: 20, color: Colors.black),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(width: 20),
+                  ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white,
+                          shape: RoundedRectangleBorder(
+                            // borderRadius: BorderRadius.circular(2),
+                            side: BorderSide(
+                                color: Color.fromARGB(255, 167, 163, 163),
+                                width: 1),
+                          ),
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 15, vertical: 20)),
+                      child: Row(
+                        children: [
+                          Column(
+                            children: [
+                              Image.asset(
+                                "../assets/duo/la_ragazza.png",
+                                width: 100,
+                                height: 150,
+                              ),
+                              Text(
+                                "la ragazza",
+                                style: TextStyle(
+                                    fontSize: 20, color: Colors.black),
+                              ),
+                            ],
+                          ),
+                        ],
+                      )),
+                ],
+              ),
+              SizedBox(
+                height: 60,
+              ),
+              ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color.fromARGB(255, 148, 228, 148),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5)),
+                    minimumSize: Size(200, 50),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "CHECK",
+                        style: TextStyle(
+                            fontSize: 20,
+                            color: Color.fromARGB(255, 15, 15, 15)),
+                      )
+                    ],
+                  )),
+            ],
+          ),
+        ),
+      ),
       bottomNavigationBar: Footer(),
     );
   }
